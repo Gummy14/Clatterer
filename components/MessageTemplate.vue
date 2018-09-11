9f08f3545667ec19cf8523ac22de603cf24e7905
Gummy14 <alex.helm@yahoo.com>
2018-09-10 21:52:39 -0400

    Image Size Fix

--- components/MessageTemplate.vue
+++ components/MessageTemplate.vue
@@ -2,7 +2,9 @@
     <div>
         <h5>Alex Helm</h5>
         <h1>{{ messageText }}</h1>
-        <img :src="imageUrl" alt="nope">
+        <div class="image-container">
+          <img class="image-style" :src="imageUrl">
+        </div>
     </div>
 </template>
 <script>
@@ -17,4 +19,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.image-container {
+  max-height:100%;
+  max-width: 100%;
+}
+.image-style {
+  max-height: 300px;
+  height: 70%;
+  width: auto;
+}
 </style>
